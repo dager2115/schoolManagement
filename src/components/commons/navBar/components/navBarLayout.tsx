b770102fac8fba3ff5cbe01e796ceea55adedb1e
dager2115 <dager2115@gmail.com>
2023-07-10 15:01:57 -0500

    change name and add navigator in logo

--- src/components/commons/navBar/components/navBarLayout.tsx
+++ src/components/commons/navBar/components/navBarLayout.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import '../navBar.scss'
+import { useNavigate } from 'react-router-dom';
 
 interface INavBarlayoutProps {
 
 }
 
 const NavBarLayout = (props: INavBarlayoutProps) => {
+
+    const navigate = useNavigate()
+
     return (
         <div className='nav-bar-container'>
-            <div className='logo-container'>
-                <h1>Students tool</h1>
+            <div style={{cursor: 'pointer'}} className='logo-container' onClick={() => navigate('/')}>
+                <h1>School Management</h1>
             </div>
         </div>
     )
